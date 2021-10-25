30d6dd1e055fc31be578e1c0f9f0f5dadc236ee6
David Hadley <davehadley@users.noreply.github.com>
2021-10-25 16:07:16 +0100

    tests fail

--- tests/test_arrow_pybind_example.py
+++ tests/test_arrow_pybind_example.py
@@ -1,4 +1,5 @@
 import tempfile
+from pathlib import Path
 
 def test_package_can_be_imported():
     import arrow_pybind_example
@@ -6,8 +7,10 @@ def test_package_can_be_imported():
 
 def test_load_csv_works():
     import arrow_pybind_example
-    with tempfile.TemporaryFile() as tmpfile:
-        csvcontent = "a,1,2.0\nb,3,4.0"
-        tempfile.write(csvcontent)
-        table = arrow_pybind_example.example_load_csv()
+    with tempfile.TemporaryDirectory() as directory:
+        filepath = Path(directory) / "example.csv"
+        with open(filepath, "wt") as tmp:
+            csvcontent = "a,1,2.0\nb,3,4.0"
+            tmp.write(csvcontent)
+        table = arrow_pybind_example.example_load_csv(str(filepath))
         assert table
